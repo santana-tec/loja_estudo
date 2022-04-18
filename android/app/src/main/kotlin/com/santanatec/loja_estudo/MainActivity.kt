45247374d12e09fcfb3a73c1cad40df7077a5b2e
santana-tec <elder.santana.tec@gmail.com>
2022-04-18 18:56:33 -0300

    Initial commit  Fairebase ok

---
+++ android/app/src/main/kotlin/com/santanatec/loja_estudo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.santanatec.loja_estudo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+ FirebaseFirestore.instance.collection('Pedidos').snapshots().listen((snapshot){
+   for (DocumentSnapshot document in snapshot.docs) {
+    print(document.data());
+  }
+ });
+  
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container());
+  }
+}
